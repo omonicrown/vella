ae75c41f86a2ce2eff417d05c6ce13b81a5a8ab5
omonicrown <45240304+omonicrown@users.noreply.github.com>
2022-06-25 12:10:32 -0700

    new changes

--- src/App.js
+++ src/App.js
@@ -6,7 +6,22 @@ import UserInfo from './embededwidget.js';
 function App() {
 
 
-  const [count, setCount] = useState([]);
+  const [count, setCount] = useState({title:'',price:'',category:'',id:''});
+  const [message, setmessage] = useState('message');
+
+  const [message2, setmessage2] = useState('messag2');
+
+  const changeMessage = React.useCallback(
+    (data) => {
+      setmessage('new mesaage '+data)
+    },
+    [message]
+);
+
+
+React.useEffect(() => {
+  setmessage('new mesaage')
+}, []);
 
 React.useEffect(() => {
     let result = fetch('https://fakestoreapi.com/products/1')
@@ -20,18 +35,20 @@ React.useEffect(() => {
   //result(data);
 console.log(count);
   const config = {
-    name : count.title,
-    price: count.price,
-    currency: count.category,
-    id:count.id
+    name : count?.title,
+    price: count?.price,
+    currency: count?.category,
+    id:count?.id
     };
 
     
   return (
     <div className="App">
       <header className="App-header">
-      
-        <UserInfo config={config}/>
+      <h1>{ message} </h1>
+      <button onClick={() => changeMessage('22')}> Click</button>
+      <h1>{ message2} </h1>
+        <UserInfo config={{name : count?.title,price: count?.price,currency: count?.category,id:count?.id}} />
       </header>
     </div>
   );
